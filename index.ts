9fee97dd207a2c704628decb727f82d37db2e791
Ilya <lord-ilja@yandex.ru>
2023-04-19 09:28:41 +0300

    typing JS-app to TypeScript-app

--- index.ts
+++ index.ts
@@ -1,62 +1,56 @@
-// структура данных склада с одеждой
-
-// ClothesWarehouse {
-// 	jackets: "empty" | number;
-// 	hats: "empty" | number;
-// 	socks: "empty" | number;
-// 	pants: "empty" | number;
-// }
-
-// структура данных склада с канцтоварами
-
-// StationeryWarehouse {
-// 	scissors: "empty" | number;
-// 	paper: "empty" | boolean;
-// }
-
-// структура данных склада с бытовой техникой
-
-// AppliancesWarehouse {
-// 	dishwashers: "empty" | number;
-// 	cookers: "empty" | number;
-// 	mixers: "empty" | number;
-// }
-
-// общая структура данных, наследует все данные из трех выше
-// + добавляет свои
-
-// TotalWarehouse {
-// 	deficit: boolean;
-// 	date: Date;
-// }
-
-// главный объект со всеми данными, должен подходить под формат TotalWarehouse
-
-const totalData = {
-	jackets: 5,
-	hats: "empty",
-	socks: "empty",
-	pants: 15,
-	scissors: 15,
-	paper: true,
-	dishwashers: 3,
-	cookers: "empty",
-	mixers: 14,
-};
-
-// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
-// и возвращает всегда строку
-// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
-// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
-
-// С данным объектом totalData строка будет выглядеть:
-// "We need this items: hats, socks, cookers"
-// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
-
-function printReport(data) {
-	return `We need this items: ${"..."}`;
-	// или
-	return "Everything fine";
+interface IClothesWarehouse {
+  jackets: 'empty' | number
+  hats: 'empty' | number
+  socks: 'empty' | number
+  pants: 'empty' | number
 }
 
-console.log(printReport(totalData));
+interface IStationeryWarehouse {
+  scissors: 'empty' | number
+  paper: 'empty' | boolean
+}
+
+interface IAppliancesWarehouse {
+  dishwashers: 'empty' | number
+  cookers: 'empty' | number
+  mixers: 'empty' | number
+}
+
+interface ITotalWarehouse
+  extends IClothesWarehouse,
+    IStationeryWarehouse,
+    IAppliancesWarehouse {
+  deficit: boolean
+  date: Date
+}
+
+const totalData: ITotalWarehouse = {
+  jackets: 5,
+  hats: 'empty',
+  socks: 'empty',
+  pants: 15,
+  scissors: 15,
+  paper: true,
+  dishwashers: 3,
+  cookers: 'empty',
+  mixers: 14,
+  deficit: true,
+  date: new Date(),
+}
+
+type TReportFn = (data: ITotalWarehouse) => string
+
+const printReport: TReportFn = (data: ITotalWarehouse): string => {
+  const arr: string[][] = Object.entries(data).filter(
+    (item) => item[1] === 'empty'
+  )
+
+  if (arr.length !== 0) {
+    const res: string = arr.map((item) => item[0]).join(', ')
+    return `We need this items: ${res}`
+  } else {
+    return 'Everything fine'
+  }
+}
+
+console.log(printReport(totalData))
