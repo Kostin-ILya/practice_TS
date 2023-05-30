a54e4b5762aec6f0849a91dd73eccbc53f5a1216
Ilya <lord-ilja@yandex.ru>
2023-05-30 16:14:22 +0300

    first task done

--- practice-61/index.ts
+++ practice-61/index.ts
@@ -6,70 +6,103 @@
 // Простыми словами: при добавлении свойства в целевой объект они должны быть
 // автоматически добавлены в зависимые (сразу подсказка от TS)
 
-const fitnessClubCenter = {
-	clubName: "Fitness club Center",
-	location: "central ave. 45, 5th floor",
-	classes: [
-		{
-			name: "yoga",
-			startsAt: "8:00 AM",
-			duration: 60,
-		},
-		{
-			name: "trx",
-			startsAt: "11:00 AM",
-			duration: 45,
-		},
-		{
-			name: "swimming",
-			startsAt: "3:00 PM",
-			duration: 70,
-		},
-	],
-	futureClasses: [
-		{
-			name: "boxing",
-			willStartsAt: "6:00 PM",
-			duration: 40,
-		},
-		{
-			name: "breath training",
-			willStartsAt: "8:00 PM",
-			duration: 30,
-		},
-	],
-	currClients: [
-		{
-			name: "John Smith",
-			age: "-",
-			gender: "male",
-			timeLeft: "1 month",
-		},
-		{
-			name: "Alise Smith",
-			age: 35,
-			gender: "female",
-			timeLeft: "3 month",
-		},
-		{
-			name: "Ann Sonne",
-			age: 24,
-			gender: "female",
-			timeLeft: "5 month",
-		},
-	],
-	exClients: [
-		{
-			name: "Tom Smooth",
-			age: 50,
-			gender: "male",
-			makeCallFor: new Date("2023-08-12"),
-		},
-	],
-	futureClients: [
-		{
-			name: "Maria",
-			makeCallFor: new Date("2023-07-10"),
-		},
-	],
-};
+interface IFitnesClubCenter {
+  clubName: string
+  location: string
+  classes: IClass[]
+  futureClasses: IFutereClass[]
+  currClients: IClient[]
+  exClients: TExClient[]
+  futureClients: TFutureClient[]
+}
+
+interface IClass {
+  name: string
+  startsAt: string
+  duration: number
+}
+interface IFutereClass extends Omit<IClass, 'startsAt'> {
+  willStartsAt: string
+}
+// type TFutereClass = Omit<IClass, 'startsAt'> & { willStartsAt: string }
+
+interface IClient {
+  name: string
+  age: number | string
+  gender: 'male' | 'female'
+  timeLeft: string
+}
+type TMakeCall = { makeCallFor: Date }
+type TExClient = Omit<IClient, 'timeLeft'> & TMakeCall
+type TFutureClient = Omit<IClient, 'timeLeft' | 'age' | 'gender'> & TMakeCall
+// interface IExClient extends Omit<IClient, 'timeLeft'> {
+// 	makeCallFor: Date
+// }
+
+const fitnessClubCenter: IFitnesClubCenter = {
+  clubName: 'Fitness club Center',
+  location: 'central ave. 45, 5th floor',
+  classes: [
+    {
+      name: 'yoga',
+      startsAt: '8:00 AM',
+      duration: 60,
+    },
+    {
+      name: 'trx',
+      startsAt: '11:00 AM',
+      duration: 45,
+    },
+    {
+      name: 'swimming',
+      startsAt: '3:00 PM',
+      duration: 70,
+    },
+  ],
+  futureClasses: [
+    {
+      name: 'boxing',
+      willStartsAt: '6:00 PM',
+      duration: 40,
+    },
+    {
+      name: 'breath training',
+      willStartsAt: '8:00 PM',
+      duration: 30,
+    },
+  ],
+  currClients: [
+    {
+      name: 'John Smith',
+      age: '-',
+      gender: 'male',
+      timeLeft: '1 month',
+    },
+    {
+      name: 'Alise Smith',
+      age: 35,
+      gender: 'female',
+      timeLeft: '3 month',
+    },
+    {
+      name: 'Ann Sonne',
+      age: 24,
+      gender: 'female',
+      timeLeft: '5 month',
+    },
+  ],
+  exClients: [
+    {
+      name: 'Tom Smooth',
+      age: 50,
+      gender: 'male',
+      makeCallFor: new Date('2023-08-12'),
+    },
+  ],
+  futureClients: [
+    {
+      name: 'Maria',
+      makeCallFor: new Date('2023-07-10'),
+    },
+  ],
+}
