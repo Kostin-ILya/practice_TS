ae53c39303904882936c177875f4836cbe1ef97c
Ilya <lord-ilja@yandex.ru>
2023-05-31 16:14:52 +0300

    other tasks done (type manipulations)

--- practice-61/form.ts
+++ practice-61/form.ts
@@ -1,13 +1,20 @@
 interface IForm {
-	login: string;
-	password: string;
+  login: string
+  password: string
 }
+
+type Validation = {
+  isValid: boolean
+  errorMsg?: string
+}
+
+type TValidationData = Record<keyof IForm, Validation>
 
 // Необходимо типизировать объект валидации
 // Учтите, что данные в форме могут расширяться и эти поля
 // должны появиться и в объекте валидации
 
-const validationData = {
-	login: { isValid: false, errorMsg: "At least 3 characters" },
-	password: { isValid: true },
-};
+const validationData: TValidationData = {
+  login: { isValid: false, errorMsg: 'At least 3 characters' },
+  password: { isValid: true },
+}
--- practice-61/slider.ts
+++ practice-61/slider.ts
@@ -1,40 +1,44 @@
 interface ISlider {
-	container?: string;
-	numberOfSlides?: number;
-	speed?: 300 | 500 | 700;
-	direction?: "horizontal" | "vertical";
-	dots?: boolean;
-	arrows?: boolean;
-	animationName?: string;
+  container?: string
+  numberOfSlides?: number
+  speed?: 300 | 500 | 700
+  direction?: 'horizontal' | 'vertical'
+  dots?: boolean
+  arrows?: boolean
+  animationName?: string
+}
+
+type TCustomSlider = Required<Omit<ISlider, 'animationName' | 'speed'>> & {
+  speed: number
 }
 
 function createSlider({
-	container = "",
-	numberOfSlides = 1,
-	speed = 300,
-	direction = "horizontal",
-	dots = true,
-	arrows = true,
+  container = '',
+  numberOfSlides = 1,
+  speed = 300,
+  direction = 'horizontal',
+  dots = true,
+  arrows = true,
 }: ISlider = {}): void {
-	console.log(container, numberOfSlides, speed, direction, dots, arrows);
+  console.log(container, numberOfSlides, speed, direction, dots, arrows)
 }
 
-createSlider();
+createSlider()
 
 // Необходимо типизировать объект настроек, который будет зависим
 // от интерфейса ISlider
 // Все поля в нем обязательны для заполнения
-const customSliderOptions = {
-	container: "id",
-	numberOfSlides: 4,
-	speed: 1100,
-	direction: "horizontal",
-	dots: true,
-	arrows: true,
-};
+const customSliderOptions: TCustomSlider = {
+  container: 'id',
+  numberOfSlides: 4,
+  speed: 1100,
+  direction: 'horizontal',
+  dots: true,
+  arrows: true,
+}
 
-function createCustomSlider(options: any): void {
-	if ("container" in options) {
-		console.log(options);
-	}
+function createCustomSlider(options: TCustomSlider): void {
+  if ('container' in options) {
+    console.log(options)
+  }
 }
